861d414177fac1f74ed559b644cb3381ee5d50e0
Wouter de Bie <172038+wouterdebie@users.noreply.github.com>
2021-04-12 20:11:54 +0000

    locast2tuner: add 0.1.11 bottle.

--- Formula/locast2tuner.rb
+++ Formula/locast2tuner.rb
@@ -5,6 +5,12 @@ class Locast2tuner < Formula
   sha256 "86efa603721573d5c7f27a7d8f978d8b9a3432c5f085cd225f116155154d5904"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/wouterdebie/homebrew-repo/releases/download/locast2tuner-0.1.11"
+    sha256 cellar: :any_skip_relocation, catalina:     "d1835974d4ee78ed767ad0d0dccd44732118be15616245e4fc93b71594328762"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "81b674978d97bfce8b4f012243f1dde4480a2975ebe7e63770de3284322726ed"
+  end
+
   depends_on "rust" => :build
 
   def install
