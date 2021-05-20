b3b828f35fcce9b7f99bfd637f9e1579743d0d3d
wouterdebie <wouterdebie@users.noreply.github.com>
2021-05-20 16:51:40 +0000

    New tag: v0.1.39
    Closes #35.
    
    Signed-off-by: Wouter de Bie <172038+wouterdebie@users.noreply.github.com>

--- Formula/locast2tuner.rb
+++ Formula/locast2tuner.rb
@@ -1,15 +1,9 @@
 class Locast2tuner < Formula
   desc "Rust implementation of locast2dvr"
   homepage "https://github.com/wouterdebie/locast2tuner"
-  url "https://github.com/wouterdebie/locast2tuner/archive/refs/tags/v0.1.38.tar.gz"
-  sha256 "eae1009a290e81e866f9a7e5be4b483bddb7b180ad95fb01e3cb7b8fda9bc20b"
+  url "https://github.com/wouterdebie/locast2tuner/archive/refs/tags/v0.1.39.tar.gz"
+  sha256 "42ccf9d22f32a6ea263237b77cf1e7dc193feffe3805b3070e1c3cc2462e0bbd"
   license "MIT"
-
-  bottle do
-    root_url "https://github.com/wouterdebie/homebrew-repo/releases/download/locast2tuner-0.1.38"
-    sha256 cellar: :any_skip_relocation, catalina:     "9661f386ccaf40face8fd058c4bbf36fafe78ae249146e4905fa6eb481f41f6b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "eeb3c64fdffb0b8d2b8acb055fd9a38148ebaf6fa26ae6e8c75e789058cc308c"
-  end
 
   depends_on "rust" => :build
 
